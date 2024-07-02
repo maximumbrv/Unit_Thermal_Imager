bcf87b5a0600fc090ff4f7d0d066806c76cc0871
Maxim Bryzgalov <maximumbrv@yandex.ru>
2024-07-02 22:04:46 +0300

    moved file name generation to method

--- uti260b.py
+++ uti260b.py
@@ -4,50 +4,52 @@ import pytesseract
 import time
 from pygrabber.dshow_graph import FilterGraph
 
-FRAME_DELAY = 40
-VIDEO_FPS = 15
+WAIT_KEY_DELAY = 10
+CAMERA_FPS = 15
 RESOLUTION = (321, 240)
+VIDEO_FOLDER = "video"
+IMAGE_FOLDER = "img"
 
 
 class Uti260b:
 
     def __init__(self):
-        self.camera = None
-        self.camera_id = None
+        self._camera = None
+        self._camera_id = None
 
     def connect(self):
         success = False
-        if self.camera is None:
-            if self.camera_id is None:
+        if self._camera is None:
+            if self._camera_id is None:
                 print("Camera not found")
             else:
                 try:
-                    self.camera = cv2.VideoCapture(self.camera_id)
+                    self._camera = cv2.VideoCapture(self._camera_id)
                 except cv2.error:
-                    print(f"Couldn't connect to camera #{self.camera_id}")
+                    print(f"Couldn't connect to camera #{self._camera_id}")
                 else:
                     success = True
         else:
-            print(f"Camera #{self.camera_id} already opened")
+            print(f"Camera #{self._camera_id} already opened")
             success = True
         return success
 
     def disconnect(self):
-        if self.camera is None:
+        if self._camera is None:
             rval = True
         else:
-            rval = self.camera.release()
+            rval = self._camera.release()
         return rval
 
     def preview(self):
-        if self.camera is None:
+        if self._camera is None:
             print("Preview unavailable, camera not connected")
             return False
 
         cv2.namedWindow("preview", flags=cv2.WINDOW_NORMAL)
 
-        if self.camera.isOpened():
-            rval, frame = self.camera.read()
+        if self._camera.isOpened():
+            rval, frame = self._camera.read()
         else:
             rval = False
             print("Couldn't read from camera")
@@ -57,20 +59,21 @@ class Uti260b:
 
         while rval:
             cv2.imshow("preview", frame)
-            rval, frame = self.camera.read()
-            key = cv2.waitKey(FRAME_DELAY)
+            rval, frame = self._camera.read()
+            key = cv2.waitKey(WAIT_KEY_DELAY)
+
             if key == 27:  # exit on ESC
                 break
+
             if key in [ord('c'), ord('C'), ord('с'), ord('С')]:
-                current_time = time.strftime("%d%m%Y-%H%M%S")
-                image_name = "img\\" + current_time + ".bmp"
+                image_name = self._get_filename_from_time('.bmp', IMAGE_FOLDER)
                 cv2.imwrite(image_name, frame)
                 print('Image captured')
+
             if key in [ord('r'), ord('R'), ord('к'), ord('К')]:
                 if not recording:
-                    current_time = time.strftime("%d%m%Y-%H%M%S")
-                    video_name = "video\\" + current_time + ".avi"
-                    writer = cv2.VideoWriter(video_name, fourcc, VIDEO_FPS, RESOLUTION[::-1])
+                    video_name = self._get_filename_from_time('.mp4', VIDEO_FOLDER)
+                    writer = cv2.VideoWriter(video_name, fourcc, CAMERA_FPS, RESOLUTION[::-1])
                     recording = True
                     print('Recording started')
                 elif writer is None:
@@ -87,7 +90,7 @@ class Uti260b:
 
     def find_camera(self):
         camera_found = False
-        available_cameras = self.get_available_cameras()
+        available_cameras = self._get_available_cameras()
 
         for index, name in available_cameras.items():
             if name == "UVC Camera":
@@ -96,7 +99,7 @@ class Uti260b:
                     status, frame = camera.read()
                     if status and frame.shape[:2] == RESOLUTION:
                         camera_found = True
-                        self.camera_id = index
+                        self._camera_id = index
                     camera.release()
 
         if camera_found:
@@ -104,18 +107,25 @@ class Uti260b:
         return False
 
     def is_found(self):
-        return self.camera_id is not None
+        return self._camera_id is not None
 
     def is_connected(self):
-        return self.camera is not None
+        return self._camera is not None
 
     @staticmethod
-    def get_available_cameras():
+    def _get_available_cameras():
         devices = FilterGraph().get_input_devices()
         available_cameras = {}
         for index, name in enumerate(devices):
             available_cameras[index] = name
         return available_cameras
+
+    @staticmethod
+    def _get_filename_from_time(extension, folder=""):
+        current_time = time.strftime("%d%m%Y-%H%M%S")
+        file_name = folder + "\\" + current_time + extension
+        return file_name
+
 
 
 if __name__ == "__main__":
