8df05e9a4879425c32b6cf079f08090537687e4a
Maxim Bryzgalov <maximumbrv@yandex.ru>
2024-07-02 20:34:28 +0300

    change find_camera method

--- uti260b.py
+++ uti260b.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import pytesseract
+from pygrabber.dshow_graph import FilterGraph
 
 
 class Uti260b:
@@ -14,19 +15,23 @@ class Uti260b:
         if self.camera is None:
             if self.camera_id is None:
                 print("Camera not found")
-            try:
-                self.camera = cv2.VideoCapture(self.camera_id)
-            except Exception:
-                print(f"Couldn't connect to camera #{self.camera_id}")
             else:
-                success = True
+                try:
+                    self.camera = cv2.VideoCapture(self.camera_id)
+                except Exception:
+                    print(f"Couldn't connect to camera #{self.camera_id}")
+                else:
+                    success = True
         else:
             print(f"Camera #{self.camera_id} already opened")
             success = True
         return success
 
     def disconnect(self):
-        rval = self.camera.release()
+        if self.camera is None:
+            rval = True
+        else:
+            rval = self.camera.release()
         return rval
 
     def preview(self):
@@ -36,7 +41,7 @@ class Uti260b:
 
         cv2.namedWindow("preview", flags=cv2.WINDOW_NORMAL)
 
-        if self.camera.isOpened():  # try to get the first frame
+        if self.camera.isOpened():
             rval, frame = self.camera.read()
             print(frame.shape)
         else:
@@ -53,21 +58,31 @@ class Uti260b:
         cv2.destroyWindow("preview")
 
     def find_camera(self):
-        id = 0
         camera_found = False
-        while not camera_found and id < 20:
-            camera = cv2.VideoCapture(id)
-            if camera.isOpened():
-                status, frame = camera.read()
-                if status and frame.shape[:2] == (321, 240):
-                    print(camera.get)
-                    camera_found = True
-                    self.camera_id = id
-                camera.release()
-            id += 1
+        available_cameras = self.get_available_cameras()
+
+        for index, name in available_cameras.items():
+            if name == "UVC Camera":
+                print(f'check camera #{index}: {name}')
+                camera = cv2.VideoCapture(index)
+                if camera.isOpened():
+                    status, frame = camera.read()
+                    if status and frame.shape[:2] == (321, 240):
+                        camera_found = True
+                        self.camera_id = index
+                    camera.release()
+
         if camera_found:
             return True
         return False
+
+    @staticmethod
+    def get_available_cameras():
+        devices = FilterGraph().get_input_devices()
+        available_cameras = {}
+        for index, name in enumerate(devices):
+            available_cameras[index] = name
+        return available_cameras
 
 
 if __name__ == "__main__":
